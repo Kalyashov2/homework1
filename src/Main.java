c9ae33ab85477593318aba6dd0f1e734813eba6d
Ivan <kalyashov-ivan@yandex.ru>
2023-07-20 20:18:14 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет, Мир");
+        var dog = 6;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        paper = paper + 4;
+        System.out.println(paper);
+        cat = cat + 4;
+        System.out.println(cat);
+
+        var dog1 = 16;
+        System.out.println(dog1);
+        var paper1 = 15;
+        System.out.println(paper1);
+        var cat1 = 44;
+        System.out.println(cat1);
+        dog1 = dog1 - 3;
+        paper1 = paper1 - 7639;
+        cat1 = cat1 - 2;
+        System.out.println(dog1);
+        System.out.println(cat1);
+        System.out.println(paper1);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
     }
 }
